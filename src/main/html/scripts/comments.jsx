7455a15f261f4ba9775132e03394bec88057b852
Clifton Craig <clifton.craig@ge.com>
2016-05-22 13:34:00 -0700

    Adds steps 2-3 of the tutorial.

--- src/main/html/scripts/comments.jsx
+++ src/main/html/scripts/comments.jsx
@@ -1,13 +1,35 @@
-// tutorial1.js
-var CommentBox = React.createClass({
+var CommentList = React.createClass({
     render: function() {
         return (
-            <div className="commentBox">
-                Hello, world! I am a CommentBox.
+            <div className="commentList">
+                Hello, world! I am a CommentList.
             </div>
         );
     }
 });
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
     <CommentBox />,
     document.getElementById('content')
