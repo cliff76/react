6f891587107ea24d88a9926140390f68205991b3
Clifton Craig <clifton.craig@ge.com>
2016-05-22 21:40:14 -0700

    Adds body-parser to Express to handle JSON POST requests. Adds logic to CommentForm and CommentBox components to handle form submits.

--- src/main/typescript/index.ts
+++ src/main/typescript/index.ts
@@ -1,4 +1,5 @@
 import express = require('express');
+import bodyParser = require('body-parser');
 let app = express();
 
 var data = [
@@ -7,9 +8,23 @@ var data = [
     {id: 3, author: "Milton Waid", text: "Hi Clifton."}
 ];
 
+//Serve static files from www
 app.use(express.static('www'));
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/api/comments', function (req, res) {
     console.log("request received");
+    res.send(data);
+});
+
+app.post('/api/comments', function (req, res) {
+    console.log("POST received: ", req.body);
+    var newComment = {id:data.length+1, author: req.body.author, text:req.body.text};
+    console.log("update received: ", newComment);
+    data.push(newComment);
     res.send(data);
 });
 
